b7664f24a0a7b5b260b46a8c26353c5f3b02271f
sarasanchez3456 <saritasanche404@gmail.com>
2026-08-28 06:35:13 -0500

    Guardo cambios antes de actualizar

--- app/src/main/java/com/example/appcrud/MainActivity.kt
+++ app/src/main/java/com/example/appcrud/MainActivity.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.appcrud.ui.theme.AppCRUDTheme
 
+import androidx.compose.ui.tooling.preview.Preview
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +40,13 @@ fun MainScreen() {
         ) {
             Text("Urbify")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    AppCRUDTheme {
+        MainScreen()
     }
 }
